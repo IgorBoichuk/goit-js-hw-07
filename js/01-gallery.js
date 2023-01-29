7019dac0d5675d840fa69735febd63e17232a3bb
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-01-29 22:34:13 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,7 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+// const myGallery = document.querySelector(".gallery");
+// console.log(myGallery);
+// instance.show(() => console.log("lightbox now visible"));
 console.log(galleryItems);
